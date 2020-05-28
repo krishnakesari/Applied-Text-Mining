0bf33a2cc8efa11f652b920ba313edd260da60ce
krishna <bkesari@outlook.com>
2020-05-28 07:59:47 -0400

    commit

--- Information Extraction.py
+++ Information Extraction.py
@@ -1,14 +1,15 @@
 # Converting unstructured text to structured text
 
 # Fields of Interest - Named Entities (Noun Phrases)
-# Named Entity Recognition
+# 1. Named Entity Recognition
 ## Identify the mention / phrase: Boundary Detection
 ## Identify the type: Tagging / classification
 
 ## Person, Organization, Location/GPE 
 
-## Relation Extraction - Identify relationships between named entities
+## 2. Relation Extraction - Identify relationships between named entities
 
-
+## 3. Co-reference resolution - Disambiguate mentions and group mentions together 
+### Pronoun resolution (If noun is referring to pronoun)
 
 
