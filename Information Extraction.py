192a0b5b92a04cfabdf9f642f0225bfb09546e42
krishna <bkesari@outlook.com>
2020-05-28 07:43:58 -0400

    commit

--- Information Extraction.py
+++ Information Extraction.py
@@ -1,2 +1,9 @@
 # Converting unstructured text to structured text
 
+# Fields of Interest - Named Entities (Noun Phrases)
+# Named Entity Recognition
+## Identify the mention / phrase: Boundary Detection
+## Identify the type: Tagging / classification
+
+
+
